04e3f70fef310b8ad459755d36386416a450972c
Esteban <esteban.gobert@me.com>
2020-03-19 20:08:41 +0100

    deezer service

--- public/src/services/deezer-service/deezer.service.ts
+++ public/src/services/deezer-service/deezer.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeezerService {
 
-  constructor() { }
+  accessToken = JSON.parse(localStorage.getItem('deezer_access_token'));
+  refreshToken = JSON.parse(localStorage.getItem('deezer_refresh_token'));
+
+  constructor(private http: HttpClient) { }
+
+  public connect() {
+    this.http.get('http://localhost:8888/deezer/login/').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
